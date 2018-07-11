81cb8fcd054563671f049a03f78e48b590c8c942
Peerapong Rithisith <p.rithisith@hotmail.com>
2018-07-11 20:34:57 +0100

    clean up code

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -3,85 +3,72 @@
 
 using namespace std;
 
-vector<int> v;
+vector<int> mergeSort(vector<int> inputVector);
+vector<int> merge(vector<int> firstVector, vector<int> secondVector);
 
-
-vector<int> mergeSort(vector<int> v);
-vector<int> merge(vector<int> a, vector<int> b);
-
-vector<int> mergeSort(vector<int> v) {
-	vector<int> a, b;
+vector<int> mergeSort(vector<int> inputVector) {
+	vector<int> firstVector, secondVector;
 	
-	if (v.size() == 1) {
-		cout << "Returning single element --> " << v[0] << endl;
-		return v;
+	if (inputVector.size() == 1) {
+		return inputVector;
 	} else {
 		
-		cout << "Splitting elements --> ";
-		for (int i = 0; i < v.size(); i++) {
-			if (i < v.size() / 2) {
-				a.push_back(v[i]);
-				cout << a[i] << " ";
+		for (int i = 0; i < inputVector.size(); i++) {
+			
+			if (i < inputVector.size() / 2) {
+				firstVector.push_back(inputVector[i]);
 			} else {
-				b.push_back(v[i]);
-				cout << b[i - (v.size() / 2)] << " ";
+				secondVector.push_back(inputVector[i]);
 			}
 		}
-		cout << endl;
 		
-		vector<int> a2 = mergeSort(a);
-		vector<int> b2 = mergeSort(b);
-		
-		return merge(a2, b2);
+		return merge(mergeSort(firstVector), mergeSort(secondVector));
 	}
 }
 
-vector<int> merge(vector<int> a, vector<int> b) {
-	vector<int> c;
+vector<int> merge(vector<int> firstVector, vector<int> secondVector) {
+	vector<int> outputVector;
 	int j = 0, k = 0;
 	
-	cout << "Forming elements --> ";
-	for (int i = 0; i < a.size() + b.size(); i++) {
+	for (int i = 0; i < firstVector.size() + secondVector.size(); i++) {
 		
-		if (j == a.size()) {
-			c.push_back(b[k]);
+		if (j == firstVector.size()) {
+			outputVector.push_back(secondVector[k]);
 			k++;
-		} else if (k == b.size()) {
-			c.push_back(a[j]);
+		} else if (k == secondVector.size()) {
+			outputVector.push_back(firstVector[j]);
 			j++;
-		}
-		else if (a[j] < b[k]) {
-			c.push_back(a[j]);
+		} else if (firstVector[j] < secondVector[k]) {
+			outputVector.push_back(firstVector[j]);
 			j++;
 		} else {
-			c.push_back(b[k]);
+			outputVector.push_back(secondVector[k]);
 			k++;
 		}
-		cout << c[i] << " ";
 	}
-	cout << endl;
 	
-	return c;
+	return outputVector;
 }
 
 int main() {
 	
-	cout << "Merge Sort Program" << endl << endl;
+	vector<int> inputVector;
+	
+	cout << "Merge Sort" << endl << endl;
 	for (int i = 10; i > 0; i--) {
-		v.push_back(i);
+		inputVector.push_back(i);
 	}
 	
 	cout << "Input: ";
 	for (int i = 0; i < 10; i++) {
-		cout << v[i] << " ";
+		cout << inputVector[i] << " ";
 	}
 	cout << endl << endl;
 	
-	cout << "Tasks:" << endl;
-	vector<int> answer = mergeSort(v);
+	vector<int> outputVector = mergeSort(inputVector);
 	
-		cout << endl << "Output:" << endl;
-		for (int i = 0; i < v.size(); i++) {
-		cout << answer[i] << " ";
+		cout << "Output: ";
+		for (int i = 0; i < outputVector.size(); i++) {
+		cout << outputVector[i] << " ";
 	}
 }
