d433f540fb0cd31acc130458b89a4e8c1d7d6e80
Peerapong Rithisith <p.rithisith@hotmail.com>
2018-07-20 23:41:48 +0100

    refactor Dijkstra's Algorithm

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -2,113 +2,115 @@
 #include <vector>
 #include <limits>
 
-using namespace std;
-
 struct Node;
 struct Edge;
 struct Graph;
 
-void calculateShortestPathFrom(Graph& graph, Node* startingNode);
-Graph createGraph();
-void createNodesUpToId(Graph& graph, int maxId);
-void createDirectedEdgeWithLength(Node* firstNode, Node* secondNode, int length);
-
 struct Node {
 	int id;
-	vector<Edge*> incidentEdges;
-	bool isExplored = false;
+	std::vector<Edge*> incidentEdges;
+	bool isExplored;
 	int shortestLength;
+	
+	Node(int id) : id(id), isExplored(false) {}
 };
 
 struct Edge {
 	Node* firstNode;
 	Node* secondNode;
 	int length;
+	
+	Edge(Node* firstNode, Node* secondNode, int length) : firstNode(firstNode), secondNode(secondNode), length(length) {}
 };
 
 struct Graph {
-	vector<Node*> nodes;
-	vector<Edge*> edges;
-	vector<Node*> visitedNodes;
-};
-
-void calculateShortestPathFrom(Graph& graph, Node* startingNode) {
-	startingNode->isExplored = true;
-	startingNode->shortestLength = 0;
-	graph.visitedNodes.push_back(startingNode);
-	bool hasNodesToVisit = true;
+	std::vector<Node*> allNodes;
+	std::vector<Edge*> edges;
+	std::vector<Node*> processedNodes;
 	
-	while(hasNodesToVisit) {
-		Node* nextNodeToVisit;
-		int shortestLength = INT_MAX;
-		hasNodesToVisit = false;
-		for (int i = 0; i < graph.visitedNodes.size(); i++) {
-			for (int j = 0; j < graph.visitedNodes[i]->incidentEdges.size(); j++) {
-				Edge* currentEdge = graph.visitedNodes[i]->incidentEdges[j];
-				Node* currentNeighbourNode = currentEdge->secondNode;
-				if (!currentNeighbourNode->isExplored) {
-					hasNodesToVisit = true;
-					int potentialShortestLength = graph.visitedNodes[i]->shortestLength + currentEdge->length;
+	Graph() {
+		createNodesUpToId(6);
+		createDirectedEdge(0, 1, 1);
+		createDirectedEdge(0, 2, 2);
+		createDirectedEdge(2, 1, 4);
+		createDirectedEdge(1, 3, 6);
+		createDirectedEdge(2, 4, 3);
+		createDirectedEdge(3, 4, 1);
+		createDirectedEdge(3, 5, 2);
+		createDirectedEdge(4, 5, 1);
+	}
+	
+	void calculateShortestPathFrom(int startingNodeId) {
+		updateNodeWithShortestLength(allNodes[startingNodeId], 0);
+		while(processedNodes.size() != allNodes.size()) {
+			Node* nodeToUpdate = NULL;
+			int shortestLength = INT_MAX;
+			searchForNodeWithShortestLength(nodeToUpdate, shortestLength);
+			if (nodeToUpdate != NULL) {
+				updateNodeWithShortestLength(nodeToUpdate, shortestLength);
+			}
+		}
+	}
+	
+	void updateNodeWithShortestLength(Node* nodeToUpdate, int shortestLength) {
+		nodeToUpdate->isExplored = true;
+		nodeToUpdate->shortestLength = shortestLength;
+		processedNodes.push_back(nodeToUpdate);
+	}
+	
+	void searchForNodeWithShortestLength(Node*& nodeToUpdate, int& shortestLength) {
+		for (int i = 0; i < processedNodes.size(); i++) {
+			for (int j = 0; j < processedNodes[i]->incidentEdges.size(); j++) {
+				Edge* currentEdge = processedNodes[i]->incidentEdges[j];
+				if (!currentEdge->secondNode->isExplored) {
+					int potentialShortestLength = processedNodes[i]->shortestLength + currentEdge->length;
 					if (potentialShortestLength < shortestLength) {
 						shortestLength = potentialShortestLength;
-						nextNodeToVisit = currentNeighbourNode;
+						nodeToUpdate = currentEdge->secondNode;
 					}
 				}
 			}
 		}
-		if (hasNodesToVisit) {
-			nextNodeToVisit->isExplored = true;
-			nextNodeToVisit->shortestLength = shortestLength;
-			graph.visitedNodes.push_back(nextNodeToVisit);
+	}
+	
+	void createNodesUpToId(int maxNodeId) {
+		for (int i = 0; i < maxNodeId; i++) {
+			allNodes.push_back(new Node(i + 1));
 		}
 	}
-}
-
-Graph createGraph() {
-	Graph graph;
-	createNodesUpToId(graph, 6);
-	createDirectedEdgeWithLength(graph.nodes[0], graph.nodes[1], 1);
-	createDirectedEdgeWithLength(graph.nodes[0], graph.nodes[2], 2);
-	createDirectedEdgeWithLength(graph.nodes[2], graph.nodes[1], 4);
-	createDirectedEdgeWithLength(graph.nodes[1], graph.nodes[3], 6);
-	createDirectedEdgeWithLength(graph.nodes[2], graph.nodes[4], 3);
-	createDirectedEdgeWithLength(graph.nodes[3], graph.nodes[4], 1);
-	createDirectedEdgeWithLength(graph.nodes[3], graph.nodes[5], 2);
-	createDirectedEdgeWithLength(graph.nodes[4], graph.nodes[5], 1);
-	return graph;
-}
-
-void createNodesUpToId(Graph& graph, int maxId) {
-	for (int i = 0; i < maxId; i++) {
-		Node* node = new Node();
-		node->id = i + 1;
-		graph.nodes.push_back(node);
+	
+	void createDirectedEdge(int firstNodeId, int secondNodeId, int length) {
+		allNodes[firstNodeId]->incidentEdges.push_back(new Edge(allNodes[firstNodeId], allNodes[secondNodeId], length));
 	}
-}
+	
+	void displayInputNodes() {
+		std::cout << "Input:" << std::endl;
+		for (int i = 0; i < allNodes.size(); i++) {
+			std::cout << "Node id: " << allNodes[i]->id << "; Incident to node(s): ";
+			for (int j = 0; j < allNodes[i]->incidentEdges.size(); j++) {
+				std::cout << allNodes[i]->incidentEdges[j]->secondNode->id << " ";
+			}
+			std::cout << std::endl;
+		}
+		std::cout << std::endl;
+	}
+	
+	void displayOutputNodes() {
+		std::cout << "Output:" << std::endl;
+		for (int i = 0; i < allNodes.size(); i++) {
+			std::cout << "Node id: " << allNodes[i]->id << "; Shortest length from node id 1: " << allNodes[i]->shortestLength << std::endl;
+		}
+	}
+};
 
-void createDirectedEdgeWithLength(Node* firstNode, Node* secondNode, int length) {
-	Edge* edge = new Edge();
-	edge->firstNode = firstNode;
-	edge->secondNode = secondNode;
-	edge->length = length;
-	firstNode->incidentEdges.push_back(edge);
+void displayTitle() {
+	std::cout << "Dijkstra's Algorithm" << std::endl << std::endl;
 }
 
 int main() {
-	Graph graph = createGraph();
-	
-	cout << "Dijkstra's Algorithm" << endl;
-	cout << endl << "Input:" << endl;
-	for (int i = 0; i < graph.nodes.size(); i++) {
-		cout << "Node id: " << graph.nodes[i]->id << "; Incident to node(s): ";
-		for (int j = 0; j < graph.nodes[i]->incidentEdges.size(); j++) {
-			cout << graph.nodes[i]->incidentEdges[j]->secondNode->id << " ";
-		}
-		cout << endl;
-	}
-	cout << endl << "Output:" << endl;
-	calculateShortestPathFrom(graph, graph.nodes[0]);
-	for (int i = 0; i < graph.nodes.size(); i++) {
-		cout << "Node id: " << graph.nodes[i]->id << "; Shortest length from node id 1: " << graph.nodes[i]->shortestLength << endl;
-	}
+	Graph* graph = new Graph();
+	displayTitle();
+	graph->displayInputNodes();
+	graph->calculateShortestPathFrom(0);
+	graph->displayOutputNodes();
 }
