ce69242973144e53bb4e9962c2ed5ae6fb3b7d4f
Peerapong Rithisith <p.rithisith@hotmail.com>
2018-07-22 22:11:09 +0100

    output nodes in the optimal set

--- MaximumWeightedIndependentSet.cpp
+++ MaximumWeightedIndependentSet.cpp
@@ -14,8 +14,9 @@ struct Node {
 };
 
 struct Graph {
-	std::vector<Node*> nodes;
+	std::vector<Node*> allNodes;
 	std::vector<int> maximumWeightedIndependentSetValues;
+	std::vector<Node*> nodesInOptimalSet;
 	
 	Graph() {
 		createWeightedNode(1, 3);
@@ -26,33 +27,56 @@ struct Graph {
 	}
 	
 	void findMaximumWeightedIndependentSet() {
+		getMaximumWeightedIndependentSetvalue();
+		reconstructOptimalSet();
+	}
+	
+	void getMaximumWeightedIndependentSetvalue() {
 		maximumWeightedIndependentSetValues.push_back(0);
-		maximumWeightedIndependentSetValues.push_back(nodes[0]->weight);
-		
-		for (int i = 1; i < nodes.size(); i++) {
-			if (maximumWeightedIndependentSetValues[i] > maximumWeightedIndependentSetValues[i - 1] + nodes[i]->weight) {
+		maximumWeightedIndependentSetValues.push_back(allNodes[0]->weight);
+		for (int i = 1; i < allNodes.size(); i++) {
+			if (maximumWeightedIndependentSetValues[i] > maximumWeightedIndependentSetValues[i - 1] + allNodes[i]->weight) {
 				maximumWeightedIndependentSetValues.push_back(maximumWeightedIndependentSetValues[i]);
 			} else {
-				maximumWeightedIndependentSetValues.push_back(maximumWeightedIndependentSetValues[i - 1] + nodes[i]->weight);
+				maximumWeightedIndependentSetValues.push_back(maximumWeightedIndependentSetValues[i - 1] + allNodes[i]->weight);
 			}
 		}
 	}
 	
+	void reconstructOptimalSet() {
+		int i = 4;
+		while (i > 0) {
+			if (maximumWeightedIndependentSetValues[i] > maximumWeightedIndependentSetValues[i - 1] + allNodes[i]->weight) {
+				i--;
+			} else {
+				nodesInOptimalSet.push_back(allNodes[i]);
+				i = i - 2;
+			}
+		}
+		if (i == 0) {
+			nodesInOptimalSet.push_back(allNodes[0]);
+		}
+	}
+	
 	void createWeightedNode(int nodeId, int weight) {
-			nodes.push_back(new Node(nodeId, weight));
+		allNodes.push_back(new Node(nodeId, weight));
 	}
 	
 	void displayInputNodes() {
 		std::cout << "Input:" << std::endl;
-		for (int i = 0; i < nodes.size(); i++) {
-			std::cout << "Node id: " << nodes[i]->id << "; Weight: " << nodes[i]->weight << std::endl;
+		for (int i = 0; i < allNodes.size(); i++) {
+			std::cout << "Node id: " << allNodes[i]->id << "; Weight: " << allNodes[i]->weight << std::endl;
 		}
 		std::cout << std::endl;
 	}
 	
 	void displayOutputNodes() {
 		std::cout << "Output:" << std::endl;
-		std::cout << "Maximum Weighted Independent Set Value: " << maximumWeightedIndependentSetValues[maximumWeightedIndependentSetValues.size() - 1];
+		std::cout << "Maximum weighted independent set value: " << maximumWeightedIndependentSetValues[maximumWeightedIndependentSetValues.size() - 1] << std::endl;
+		std::cout << "Node id(s) in optimal set: ";
+		for (int i = 0; i < nodesInOptimalSet.size(); i++) {
+			std::cout << nodesInOptimalSet[i]->id << " ";
+		}
 	}
 };
 
