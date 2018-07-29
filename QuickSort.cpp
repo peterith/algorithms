33f78a23087e3eea8e4ea5fc3c020ff7c85a4ef5
Peerapong Rithisith <p.rithisith@hotmail.com>
2018-07-29 15:41:57 +0100

    tail-call optimise Quick Sort

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -1,64 +1,81 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
+std::vector<int> inputVector;
 
-void quickSort(vector<int>& inputVector, int leftLimit, int rightLimit);
-int choosePivotPosition(int leftLimit);
-int partitionAroundPivotAndReturnBoundary(vector<int>& inputVector, int pivotPosition, int leftLimit, int rightLimit);
-void swapPosition(vector<int>& inputVector, int firstPosition, int secondPosition);
+void quickSort(int low, int high);
+int selectPivot(int low);
+int partitionAndReturnNewPivot(int pivot, int low, int high);
+void swapPosition(int firstPosition, int secondPosition);
 
-void quickSort(vector<int>& inputVector, int leftLimit, int rightLimit) {
-	if (rightLimit - leftLimit <= 1) {
-		return;
+void quickSort(int low, int high) {
+	while (low < high) {
+		int pivot = selectPivot(low);
+		int newPivot = partitionAndReturnNewPivot(pivot, low, high);
+		if (newPivot - low < high - newPivot) {
+			quickSort(low, newPivot - 1);
+			low = newPivot + 1;
+			
+		} else {
+			quickSort(newPivot + 1, high);
+			high = newPivot - 1;
+		}
 	}
-	int pivotPosition = choosePivotPosition(leftLimit);
-	int newPivotPosition = partitionAroundPivotAndReturnBoundary(inputVector, pivotPosition, leftLimit, rightLimit);
-	quickSort(inputVector, leftLimit, newPivotPosition - 1);
-	quickSort(inputVector, newPivotPosition + 1, rightLimit);
 }
 
-int choosePivotPosition(int leftLimit) {
-	return leftLimit;
+int selectPivot(int low) {
+	return low;
 }
 
-int partitionAroundPivotAndReturnBoundary(vector<int>& inputVector, int pivotPosition, int leftLimit, int rightLimit) {
-	int pivot = inputVector[pivotPosition];
-	int partitionBoundary = leftLimit;
-	
-	for (int i = leftLimit; i <= rightLimit; i++) {
-		if (i == pivotPosition) {
+int partitionAndReturnNewPivot(int pivot, int low, int high) {
+	int partitionBoundary = low;
+	for (int i = low; i <= high; i++) {
+		if (i == pivot) {
 			partitionBoundary++;
-		} else if (inputVector[i] < pivot) {
-			swapPosition(inputVector, i, partitionBoundary);
+		} else if (inputVector[i] < inputVector[pivot]) {
+			swapPosition(i, partitionBoundary);
 			partitionBoundary++;
 		}
 	}
-	swapPosition(inputVector, pivotPosition, partitionBoundary - 1);
+	swapPosition(pivot, partitionBoundary - 1);
 	return partitionBoundary - 1;
 }
 
-void swapPosition(vector<int>& inputVector, int firstPosition, int secondPosition) {
+void swapPosition(int firstPosition, int secondPosition) {
 	int temp = inputVector[firstPosition];
 	inputVector[firstPosition] = inputVector[secondPosition];
 	inputVector[secondPosition] = temp;
 }
 
-int main() {
-	vector<int> inputVector;
-	
-	cout << "Quick Sort" << endl << endl;
+void displayTitle() {
+	std::cout << "Quick Sort" << std::endl << std::endl;
+}
+
+void createVector() {
 	for (int i = 10; i > 0; i--) {
 		inputVector.push_back(i);
 	}
-	cout << "Input: ";
+}
+
+void displayInput() {
+	std::cout << "Input: ";
 	for (int i = 0; i < 10; i++) {
-		cout << inputVector[i] << " ";
+		std::cout << inputVector[i] << " ";
 	}
-	cout << endl << endl;
-	quickSort(inputVector, 0, inputVector.size() - 1);
-	cout << "Output: ";
+	std::cout << std::endl << std::endl;
+}
+
+void displayOutput() {
+	std::cout << "Output: ";
 	for (int i = 0; i < 10; i++) {
-		cout << inputVector[i] << " ";
+		std::cout << inputVector[i] << " ";
 	}
+}
+
+int main() {
+	displayTitle();
+	createVector();
+	displayInput();
+	quickSort(0, inputVector.size() - 1);
+	displayOutput();
 }
